57bed56e520357768e5b019951fd2194191faf04
SaraRosad <sararos900@gmail.com>
2023-03-07 16:59:43 -0400

    3-7-2023

--- public/assets/js/mapbox.js
+++ public/assets/js/mapbox.js
@@ -1,66 +1,65 @@
-
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3JvczI3IiwiYSI6ImNsZXlveXdjZDBqNGczeXFraWY1dXp6MzYifQ.PESgUxpygsjZL07JvQFxaQ';
 var currentMarkers=[];
 const locationMap = [];
 const coordinates = document.getElementById('coordinates');
-  var response = '';
+var response = '';
 var dataGeoCoordinates = [];
-  const map = new mapboxgl.Map({
-      container: 'map',
-      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [-96, 37.8],
-      zoom: 3
-  });
-    // Add the control to the map.
-    map.addControl(
-        new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl
-        })
-        );
-  const marker = new mapboxgl.Marker({
-       color: '#3bb2d0',
-       draggable: true,
-     });
 
-   map.on('click', (e) => {
-      const lngLat = e.lngLat;
+const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [-96, 37.8],
+    zoom: 3
+});
+// Add the control to the map.
+map.addControl(
+    new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+    })
+);
+const marker = new mapboxgl.Marker({
+    color: '#3bb2d0',
+    draggable: true,
+});
+
+map.on('click', (e) => {
+    const lngLat = e.lngLat;
     marker.setLngLat([lngLat.lng, lngLat.lat]).addTo(map);
-          position(lngLat.lng, lngLat.lat, marker);
-  });
+        position(lngLat.lng, lngLat.lat, marker);
+});
 
-  marker.on('dragend', searchCoord);
+marker.on('dragend', searchCoord);
 
 
 function position(lng, lat, marker){
   // tmp marker
    currentMarkers.push(marker);
 
-if (currentMarkers.length === 2){
-    max  = currentMarkers.length;
-  currentMarkers[max].remove();
-}
+    if (currentMarkers.length === 2){
+        max  = currentMarkers.length;
+    currentMarkers[max].remove();
+    }
 
-if(currentMarkers.length <= 2){
-  fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/'+lng+','+lat+'.json?access_token='+mapboxgl.accessToken)
-    .then(response => response.json())
-    .then(data => {
-        dataGeoCoordinates.push(data);
-       document.getElementById('features').innerHTML = JSON.stringify(
-            data,
-            null,
-            2
-          );
-    });
-}
-
+    if(currentMarkers.length <= 2){
+        fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/'+lng+','+lat+'.json?access_token='+mapboxgl.accessToken)
+        .then(response => response.json())
+        .then(data => {
+            dataGeoCoordinates.push(data);
+            document.getElementById('features').innerHTML = JSON.stringify(
+                data,
+                null,
+                2
+            );
+        });
+    }
 }
 
 function searchCoord(){
- const coord = marker.getLngLat();
- dataGeoCoordinates.length = 0;
- response = fetch(
+    const coord = marker.getLngLat();
+    dataGeoCoordinates.length = 0;
+    response = fetch(
     'https://api.mapbox.com/geocoding/v5/mapbox.places/'+coord.lng+','+coord.lat+'.json?access_token='+mapboxgl.accessToken
     )
     .then(response => response.json())
@@ -78,6 +77,4 @@ function searchCoord(){
 
         document.getElementById('features').innerHTML = dataGeoCoordinates;
     });
-
-
 }
